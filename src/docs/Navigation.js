da229a214fc49d4c8e33259b4d7d1fbeb81aa6da
David Glass <davidmglass@gmail.com>
2018-10-26 15:52:53 -0400

    Implemented filter for navigation.

--- src/docs/Navigation.js
+++ src/docs/Navigation.js
@@ -1,32 +1,87 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Navigation = ({components}) => {
-  return (
-    <div className="navmenu">
-      <h1>React Components</h1>
-      <div className={'navigation--search'}>
-        <input className={'navigation--input'} placeholder={'Filter by name'}></input>
-      </div>
-      <ul className="navigation">
-        <li className={'navigation--listItem'}>
-          <a className={'navigation--link'} href={`/#`}>Components</a>
-        </li>
+class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      components: this.props.components
+    }
+  }
+
+
+  handleFilterComponents = ev => {
+    let { components } = this.props;
+
+    let value = ev.target.value.trim().toLowerCase();
+
+    if (value) {
+      components = this.props.components.filter( component => {
+          return component.toLowerCase().indexOf(ev.target.value) > -1;
+      });
+    }
+
+    this.setState({
+      components: components
+    });
+  }
+
+
+  render() {
+
+    return (
+      <div className="navmenu">
+        <h1>React Components</h1>
+        <div className={'navigation--search'}>
+          <input className={'navigation--input'} placeholder={'Filter by name'} onChange={this.handleFilterComponents}></input>
+        </div>
         <ul className="navigation">
-          {
-            components.map( name => {
-              return (
-                <li className={'navigation--listItem'} key={name}>
-                  <a className={'navigation--link'} href={`#${name}`}>{name}</a>
-                </li>
-              )
-            })
-          }
+          <li className={'navigation--listItem'}>
+            <a className={'navigation--link'} href={`/#`}>Components</a>
+          </li>
+          <ul className="navigation">
+            {
+              this.state.components.map( name => {
+                return (
+                  <li className={'navigation--listItem'} key={name}>
+                    <a className={'navigation--link'} href={`#${name}`}>{name}</a>
+                  </li>
+                )
+              })
+            }
+          </ul>
         </ul>
-      </ul>
-    </div>
-  )
+      </div>
+    );
+  }
 }
+
+// const Navigation = ({components}) => {
+//   return (
+//     <div className="navmenu">
+//       <h1>React Components</h1>
+//       <div className={'navigation--search'}>
+//         <input className={'navigation--input'} placeholder={'Filter by name'}></input>
+//       </div>
+//       <ul className="navigation">
+//         <li className={'navigation--listItem'}>
+//           <a className={'navigation--link'} href={`/#`}>Components</a>
+//         </li>
+//         <ul className="navigation">
+//           {
+//             components.map( name => {
+//               return (
+//                 <li className={'navigation--listItem'} key={name}>
+//                   <a className={'navigation--link'} href={`#${name}`}>{name}</a>
+//                 </li>
+//               )
+//             })
+//           }
+//         </ul>
+//       </ul>
+//     </div>
+//   )
+// }
 
 Navigation.propTypes = {
   components: PropTypes.array.isRequired
