0c95537c99cbf9170e95208e8c1c5c63dffa65b5
David Glass <davidmglass@gmail.com>
2018-10-27 12:31:43 -0400

    Replaced remaining EyeIcon component instances with the more generic Icons component. Removed the EyeIcon component.

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
-export {default as EyeIcon} from './EyeIcon';
 export {default as ProgeressBar} from './ProgressBar';
 export {default as Label} from './Label';
 export {default as TextInput} from './TextInput';
 export {default as PasswordInput} from './PasswordInput';
 export {default as RegistrationForm} from './RegistrationForm';
 export {default as Input} from './Input';
+export {default as Icons} from './Icons';
